1f110bab381f11521069b634ed7766d11d0924d3
Roman Birg <romanbirg@gmail.com>
2011-09-05 17:58:15 -0500

    updated to new camera wrapper

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -24,8 +24,9 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 
 LOCAL_SHARED_LIBRARIES := liblog
 
-LOCAL_MODULE := lights.default
+LOCAL_MODULE := lights.aries
 
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
